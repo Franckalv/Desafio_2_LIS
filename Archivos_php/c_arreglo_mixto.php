0332029a5ad77e746d57cf55f9857b949309ac04
Franckalv <78367187+Franckalv@users.noreply.github.com>
2024-04-16 23:19:00 -0600

    Ejercicio 2: primer avance

---
+++ Archivos_php/c_arreglo_mixto.php
@@ -0,0 +1,10 @@
+<?php
+$matriz_estudiantes = array(
+    "basico" => 
+    array("ingles"=> 25, "frances" => 10, "mandarin" => 8, "ruso" =>12, "portugues" => 30, "japones" => 90),
+    "intermedio" => 
+    array("ingles"=> 15, "frances" => 5, "mandarin" => 4, "ruso" => 8, "portugues" => 15, "japones" => 25),
+    "avanzado" => 
+    array("ingles"=> 10, "frances" => 2, "mandarin" => 1, "ruso" => 4, "portugues" => 10, "japones" => 67); 
+);
+?>
