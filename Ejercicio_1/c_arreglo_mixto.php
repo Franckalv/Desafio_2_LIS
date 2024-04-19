d78f63360f4a16cc6b2106c65559459de1942c0e
Franckalv <78367187+Franckalv@users.noreply.github.com>
2024-04-19 00:15:54 -0600

    Segundo avance (estilos y funcionalidad de "a")

---
+++ Ejercicio_1/c_arreglo_mixto.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $matriz_estudiantes = array(
+            "basico" => 
+            array("ingles"=> 25, "frances" => 10, "mandarin" => 8, "ruso" =>12, "portugues" => 30, "japones" => 90),
+            "intermedio" => 
+            array("ingles"=> 15, "frances" => 5, "mandarin" => 4, "ruso" => 8, "portugues" => 15, "japones" => 25),
+            "avanzado" => 
+            array("ingles"=> 10, "frances" => 2, "mandarin" => 1, "ruso" => 4, "portugues" => 10, "japones" => 67)
+        );
+
+        echo "Hola mundo";
+    ?>
+</body>
+</html>
